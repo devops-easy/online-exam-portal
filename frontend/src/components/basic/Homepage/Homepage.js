a75803b75be26653604d55e30b79ea47a238543f
DevopsEasy <48271676+DevopsEasy@users.noreply.github.com>
2023-08-03 17:29:59 +0530

    Update Homepage.js

--- frontend/src/components/basic/Homepage/Homepage.js
+++ frontend/src/components/basic/Homepage/Homepage.js
@@ -9,19 +9,20 @@ import logoImg from './main.jpg';
 
 function Homepage(){
   console.log("on home page");
-  if(Auth.retriveToken() && Auth.retriveToken()!=='undefined'){
-    return (<Navigate to='/home'/>);
-  }
-  else {
-    return (
-      <div>
-          <div className="parallax">
-            <HomepageHeader title='Exam Portal' img={logoImg}/>
-            <Login />
-          </div>
-      </div>
-    );
-  }
+  return (<Navigate to='/home'/>);
+  // if(Auth.retriveToken() && Auth.retriveToken()!=='undefined'){
+  //   return (<Navigate to='/home'/>);
+  // }
+  // else {
+  //   return (
+  //     <div>
+  //         <div className="parallax">
+  //           <HomepageHeader title='Exam Portal' img={logoImg}/>
+  //           <Login />
+  //         </div>
+  //     </div>
+  //   );
+  // }
 }
 
 export default Homepage;
